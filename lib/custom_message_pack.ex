5bb307c4f2c848151dbdcf0a0831cce2370ac769
Milen Donchev <milen_d@abv.bg>
2018-05-06 17:25:52 +0300

    Implement fix strings and fix tests

--- lib/custom_message_pack.ex
+++ lib/custom_message_pack.ex
@@ -3,9 +3,18 @@ defmodule CustomMessagePack do
 
   def unpack(binary) do
     case binary do
-      <<0xC0>> -> nil
-      <<0xC2>> -> false
-      <<0xC3>> -> true
+      <<0xC0>> ->
+        nil
+
+      <<0xC2>> ->
+        false
+
+      <<0xC3>> ->
+        true
+
+      # 31 bytes for fixstr
+      <<n, x::binary>> when is_integer(n) and n >= 160 and n <= 191 ->
+        x
     end
   end
 end
--- test/custom_message_pack_test.exs
+++ test/custom_message_pack_test.exs
@@ -84,18 +84,16 @@ defmodule CustomMessagePackTest do
     assert CustomMessagePack.unpack(<<0xCBBFE6666666666666>>) == -0.7
   end
 
-  test "can unpack strings" do
-    assert CustomMessagePack.unpack(<<0xA130>>) == "0"
-    assert CustomMessagePack.unpack(<<0xA139>>) == "9"
-    # from 3a to 40
-    assert CustomMessagePack.unpack(<<0xA73A3B3C3D3E3F40>>) == ":;<=>?@"
-    assert CustomMessagePack.unpack(<<0xA141>>) == "A"
-    assert CustomMessagePack.unpack(<<0xA15A>>) == "Z"
-    # from 5b to 60
-    assert CustomMessagePack.unpack(<<0xA65B5C5D5E5F60>>) == "[\\]^_`"
-    assert CustomMessagePack.unpack(<<0xA161>>) == "a"
-    assert CustomMessagePack.unpack(<<0xA17A>>) == "z"
-    assert CustomMessagePack.unpack(<<0xA463646430>>) == "cdd0"
-    assert CustomMessagePack.unpack(<<0xA56364646439>>) == "cddd9"
+  test "can unpack fix strings" do
+    assert CustomMessagePack.unpack(<<0xA0>>) == ""
+    assert CustomMessagePack.unpack(<<0xA1, 48>>) == "0"
+    assert CustomMessagePack.unpack(<<0xA1, 57>>) == "9"
+    assert CustomMessagePack.unpack(<<0xA7, 58, 59, 60, 61, 62, 63, 64>>) == ":;<=>?@"
+    assert CustomMessagePack.unpack(<<0xA1, 65>>) == "A"
+    assert CustomMessagePack.unpack(<<0xA1, 90>>) == "Z"
+    assert CustomMessagePack.unpack(<<0xA6, 91, 92, 93, 94, 95, 96>>) == "[\\]^_`"
+    assert CustomMessagePack.unpack(<<0xA1, 97>>) == "a"
+    assert CustomMessagePack.unpack(<<0xA1, 122>>) == "z"
+    assert CustomMessagePack.unpack(<<0xA4, 99, 100, 101, 48>>) == "cde0"
   end
 end
