300d411d5aebcf286e4bbcf641d437ec55f6b859
Milen Donchev <milen_d@abv.bg>
2018-05-06 17:41:00 +0300

    Fix tests

--- test/custom_message_pack_test.exs
+++ test/custom_message_pack_test.exs
@@ -14,74 +14,74 @@ defmodule CustomMessagePackTest do
   test "can unpack unsigned integers" do
     # 8 bit
     # (1100 1100) 0000 0000
-    assert CustomMessagePack.unpack(<<0xCC00>>) == 0
+    assert CustomMessagePack.unpack(<<0xCC, 0>>) == 0
     # (1100 1100) 0001 0001
-    assert CustomMessagePack.unpack(<<0xCC11>>) == 17
+    assert CustomMessagePack.unpack(<<0xCC, 17>>) == 17
     # (1100 1100) 0111 1111 ->
-    assert CustomMessagePack.unpack(<<0xCC7F>>) == 127
+    assert CustomMessagePack.unpack(<<0xCC, 127>>) == 127
     # (1100 1100) 1000 0000 ->
-    assert CustomMessagePack.unpack(<<0xCC80>>) == 128
+    assert CustomMessagePack.unpack(<<0xCC, 128>>) == 128
     # (1100 1100) 1111 1111 ->
-    assert CustomMessagePack.unpack(<<0xCCFF>>) == 255
+    assert CustomMessagePack.unpack(<<0xCC, 255>>) == 255
 
     # 16 bit
     # (1100 1101) 0000 0001 0000 0000 ->
-    assert CustomMessagePack.unpack(<<0xCD0100>>) == 256
+    assert CustomMessagePack.unpack(<<0xCD, 1, 0>>) == 256
     # (1100 1101) 0001 0000 0101 1110 ->
-    assert CustomMessagePack.unpack(<<0xCD105E>>) == 4190
+    assert CustomMessagePack.unpack(<<0xCD, 16, 94>>) == 4190
     # (1100 1101) 1111 1111 1111 1111 ->
-    assert CustomMessagePack.unpack(<<0xCDFFFF>>) == 65535
+    assert CustomMessagePack.unpack(<<0xCD, 255, 255>>) == 65535
 
     # 32 bit
     # (1100 1110) 0000 0000 0000 0001 0000 0000 0000 0000 ->
-    assert CustomMessagePack.unpack(<<0xCE00010000>>) == 65536
+    assert CustomMessagePack.unpack(<<0xCE, 0, 1, 0, 0>>) == 65536
     # (1100 1110) 1111 1111 1111 1111 1111 1111 1111 1111 ->
-    assert CustomMessagePack.unpack(<<0xCEFFFFFFFF>>) == 4_294_967_295
+    assert CustomMessagePack.unpack(<<0xCE, 255, 255, 255, 255>>) == 4_294_967_295
 
     # 64 bit
-    assert CustomMessagePack.unpack(<<0xCF0000000100000000>>) == 4_294_967_296
-    assert CustomMessagePack.unpack(<<0xCF0000006968827AF4>>) == 452_724_947_700
+    assert CustomMessagePack.unpack(<<0xCF, 0, 0, 0, 1, 0, 0, 0, 0>>) == 4_294_967_296
+    assert CustomMessagePack.unpack(<<0xCF, 0, 0, 0, 105, 104, 130, 122, 244>>) == 452_724_947_700
   end
 
   test "can unpack signed integers" do
     # 8 bit
     # (1101 0000) 1111 1111
-    assert CustomMessagePack.unpack(<<0xD0FF>>) == -1
+    assert CustomMessagePack.unpack(<<0xD0, 255>>) == -1
     # (1101 0000) 1110 0000
-    assert CustomMessagePack.unpack(<<0xD0E0>>) == -32
+    assert CustomMessagePack.unpack(<<0xD0, 224>>) == -32
     # (1101 0000) 1101 1111 ->
-    assert CustomMessagePack.unpack(<<0xD0DF>>) == -33
+    assert CustomMessagePack.unpack(<<0xD0, 223>>) == -33
     # (1101 0000) 1000 0001 ->
-    assert CustomMessagePack.unpack(<<0xD081>>) == -127
+    assert CustomMessagePack.unpack(<<0xD0, 129>>) == -127
 
     # 16 bit
     # (1101 0001) 1111 1111 1000 0000 ->
-    assert CustomMessagePack.unpack(<<0xD1FF80>>) == -128
+    assert CustomMessagePack.unpack(<<0xD1, 255, 128>>) == -128
     # (1101 0001) 1111 1111 0111 1111 ->
-    assert CustomMessagePack.unpack(<<0xD1FF7F>>) == -129
+    assert CustomMessagePack.unpack(<<0xD1, 255, 127>>) == -129
     # (1100 0001) 1111 1111 0000 0000 ->
-    assert CustomMessagePack.unpack(<<0xD1FF00>>) == -256
+    assert CustomMessagePack.unpack(<<0xD1, 255, 0>>) == -256
     # (1100 0001) 1000 0000 0000 0001 ->
-    assert CustomMessagePack.unpack(<<0xD18001>>) == -32767
+    assert CustomMessagePack.unpack(<<0xD1, 128, 1>>) == -32767
 
     # 32 bit
     # (1100 0010) 1111 1111 1111 1111 1000 0000 0000 0000 ->
-    assert CustomMessagePack.unpack(<<0xD2FFFF8000>>) == -32768
+    assert CustomMessagePack.unpack(<<0xD2, 255, 255, 128, 0>>) == -32768
     # (1100 0010) 1111 1111 1111 1111 0111 1111 1111 1111 ->
-    assert CustomMessagePack.unpack(<<0xD2FFFF7FFF>>) == -32769
+    assert CustomMessagePack.unpack(<<0xD2, 255, 255, 127, 255>>) == -32769
     # (1100 0010) 1111 1111 1111 1111 0000 0000 0000 0000 ->
-    assert CustomMessagePack.unpack(<<0xD2FFFF0000>>) == -65536
+    assert CustomMessagePack.unpack(<<0xD2, 255, 255, 0, 0>>) == -65536
     # (1100 0010) 1000 0000 0000 0000 0000 0000 0000 0001 ->
-    assert CustomMessagePack.unpack(<<0xD280000001>>) == -2_147_483_647
+    assert CustomMessagePack.unpack(<<0xD2, 128, 0, 0, 1>>) == -2_147_483_647
 
     # 64 bit
-    assert CustomMessagePack.unpack(<<0xD3FFFFFFFF80000000>>) == -2_147_483_648
-    assert CustomMessagePack.unpack(<<0xD3FFFFFFFF00000000>>) == -4_294_967_296
+    assert CustomMessagePack.unpack(<<0xD3, 255, 255, 255, 255, 128, 0, 0, 0>>) == -2_147_483_648
+    assert CustomMessagePack.unpack(<<0xD3, 255, 255, 255, 255, 0, 0, 0, 0>>) == -4_294_967_296
   end
 
   test "can unpack double floating point numbers" do
-    assert CustomMessagePack.unpack(<<0xCB4013C28F5C28F5C3>>) == 4.94
-    assert CustomMessagePack.unpack(<<0xCBBFE6666666666666>>) == -0.7
+    assert CustomMessagePack.unpack(<<0xCB, 64, 19, 194, 143, 92, 40, 245, 195>>) == 4.94
+    assert CustomMessagePack.unpack(<<0xCB, 191, 230, 102, 102, 102, 102, 102, 102>>) == -0.7
   end
 
   test "can unpack fix strings" do
