518d91c9615928d0e60bd60371fd8b2ddd60d59a
Milen Donchev <milen_d@abv.bg>
2018-05-06 16:07:25 +0300

    Add string and double tests

--- test/custom_message_pack_test.exs
+++ test/custom_message_pack_test.exs
@@ -78,4 +78,24 @@ defmodule CustomMessagePackTest do
     assert CustomMessagePack.unpack(<<0xD3FFFFFFFF80000000>>) == -2_147_483_648
     assert CustomMessagePack.unpack(<<0xD3FFFFFFFF00000000>>) == -4_294_967_296
   end
+
+  test "can unpack double floating point numbers" do
+    assert CustomMessagePack.unpack(<<0xCB4013C28F5C28F5C3>>) == 4.94
+    assert CustomMessagePack.unpack(<<0xCBBFE6666666666666>>) == -0.7
+  end
+
+  test "can unpack strings" do
+    assert CustomMessagePack.unpack(<<0xA130>>) == "0"
+    assert CustomMessagePack.unpack(<<0xA139>>) == "9"
+    # from 3a to 40
+    assert CustomMessagePack.unpack(<<0xA73A3B3C3D3E3F40>>) == ":;<=>?@"
+    assert CustomMessagePack.unpack(<<0xA141>>) == "A"
+    assert CustomMessagePack.unpack(<<0xA15A>>) == "Z"
+    # from 5b to 60
+    assert CustomMessagePack.unpack(<<0xA65B5C5D5E5F60>>) == "[\\]^_`"
+    assert CustomMessagePack.unpack(<<0xA161>>) == "a"
+    assert CustomMessagePack.unpack(<<0xA17A>>) == "z"
+    assert CustomMessagePack.unpack(<<0xA463646430>>) == "cdd0"
+    assert CustomMessagePack.unpack(<<0xA56364646439>>) == "cddd9"
+  end
 end
